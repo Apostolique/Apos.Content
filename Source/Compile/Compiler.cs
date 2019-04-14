feff2c1231b2c9ea02f6a45d14acb39cd2124abb
Jean-David Moisan <vynecompiler@gmail.com>
2019-04-14 03:16:05 -0400

    Build should create folder if it doesn't exist

--- Source/Compile/Compiler.cs
+++ Source/Compile/Compiler.cs
@@ -12,6 +12,7 @@ namespace Apos.Content.Compile {
         /// The paths can be relative or absolute.
         /// </summary>
         public virtual void Build(string inputPath, string outputPath, Settings<T> settings) {
+            Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
             using (FileStream input = new FileStream(inputPath, FileMode.Open))
             using (FileStream output = new FileStream(outputPath, FileMode.Create)) {
                 Build(input, output, settings);
