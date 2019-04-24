e2cdcc3e1e839be86974a48e72ef1cef3d25a16d
Jean-David Moisan <vynecompiler@gmail.com>
2019-04-23 23:50:55 -0400

    Binary content

---
+++ Source/Compile/CompileBinary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Optional;
+
+namespace Apos.Content.Compile {
+    /// <summary>
+    /// A binary content only contains bytes.
+    /// </summary>
+    public class CompileBinary : Compiler<byte[], Settings<byte[]>> {
+        /// <summary>
+        /// Builds a binary content.
+        /// </summary>
+        public override void Build(Stream input, Stream output, Settings<byte[]> settings) {
+            using (BinaryWriter bw = new BinaryWriter(output))
+            using (MemoryStream ms = new MemoryStream()) {
+                input.CopyTo(ms);
+                byte[] bytes = ms.ToArray();
+                bw.Write(bytes.Length);
+                bw.Write(bytes);
+            }
+        }
+    }
+}
---
+++ Source/Read/ReadBinary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Optional;
+
+namespace Apos.Content.Read {
+    /// <summary>
+    /// A binary content is simply a byte array.
+    /// </summary>
+    public class ReadBinary : Reader<byte[]> {
+        /// <summary>
+        /// Reads a binary content.
+        /// </summary>
+        /// <returns>
+        /// Returns something only if the content can be parsed.
+        /// </returns>
+        public override Option<byte[]> Read(Stream input, Context context) {
+            using (BinaryReader br = new BinaryReader(input)) {
+                int count = br.ReadInt32();
+                return Option.Some(br.ReadBytes(count));
+            }
+        }
+    }
+}
