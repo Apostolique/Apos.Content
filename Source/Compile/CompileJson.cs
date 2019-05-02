8773b7a80284cb0dd8cd8ee80423e2e91d462439
Jean-David Moisan <vynecompiler@gmail.com>
2019-05-02 01:48:50 -0400

    New json content

---
+++ Source/Compile/CompileJson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using Optional;
+
+namespace Apos.Content.Compile {
+    /// <summary>
+    /// Text file in the json format.
+    /// </summary>
+    public class CompileJson<T> : Compiler<T, Settings<T>> {
+        /// <summary>
+        /// Builds a string content.
+        /// </summary>
+        public override void Build(Stream input, Stream output, Settings<T> settings) {
+            using (StreamReader sr = new StreamReader(input))
+            using (BsonDataWriter bw = new BsonDataWriter(output)) {
+                JsonSerializer serializer = new JsonSerializer();
+                string text = sr.ReadToEnd();
+                serializer.Serialize(bw, text);
+            }
+        }
+    }
+}
---
+++ Source/Read/ReadJson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using Optional;
+
+namespace Apos.Content.Read {
+    /// <summary>
+    /// A json content is simply a file with text.
+    /// </summary>
+    public class ReadJson<T> : Reader<T> {
+        /// <summary>
+        /// Reads some json content into a specific class type.
+        /// </summary>
+        /// <returns>
+        /// Returns something only if the content can be parsed.
+        /// </returns>
+        public override Option<T> Read(Stream input, Context context) {
+            using (BsonDataReader br = new BsonDataReader(input)) {
+                JsonSerializer serializer = new JsonSerializer();
+                return Option.Some(serializer.Deserialize<T>(br));
+            }
+        }
+    }
+}
